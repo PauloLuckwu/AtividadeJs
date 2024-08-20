5f22cf504dafd3d9231d6d062f18029e039c9f30
PauloLuckwu <164958945+PauloLuckwu@users.noreply.github.com>
2024-08-19 21:46:11 -0300

    Entrega de atividade

---
+++ index.js
@@ -0,0 +1,6 @@
+const nome = prompt('Digite seu nome')
+const sobrenome = prompt('Digite seu sobrenome')
+const anonasci = parseInt(prompt('Digite seu ano de nascimento'))
+const ano = 2024
+
+alert(`Olá dr(a) ${nome} ${sobrenome}, você tem ${ano-anonasci} anos`)
